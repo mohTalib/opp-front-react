8ec4085ef9748c0c9f393d7c2dbd071da5999ff5
mohTalib <107358717+mohTalib@users.noreply.github.com>
2023-10-22 00:54:52 +0300

    Update voloid.js

--- src/containers/voloid/voloid.js
+++ src/containers/voloid/voloid.js
@@ -1,20 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom'; 
+import { useParams } from 'react-router-dom';
 import "./style.css";
 
 const Voloid = () => {
-    const { id: oppscoId } = useParams(); 
-    const [opp, setOpp] = useState(null);
+  const { id: oppscoId } = useParams();
+  const [opp, setOpp] = useState(null);
 
-    const getList_Opps_volo = async () => {
-        let response = await fetch(`/oppsvo/${oppscoId}`)
-        let data = await response.json()
-        setOpp(data)
+  const getList_Opps_volo = async () => {
+    try {
+      const response = await fetch(`https://dj-front.onrender.com/oppsvo/${oppscoId}`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setOpp(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
     }
+  };
 
-    useEffect(() => {
-        getList_Opps_volo();
-    }, [oppscoId]);
+  useEffect(() => {
+    getList_Opps_volo();
+  }, [oppscoId]);
 
 
     function updateCountdown() {
@@ -56,21 +63,21 @@ const Voloid = () => {
     return (
         <><div data-aos="fade" data-aos-easing="ease-in" data-aos-duration="1000">
         </div><div className="d-flex flex-column justify-content-center">
-                    <img src={opp?.img5} alt="" className="img-det d-flex flex-wrap" />
-                <div className="ph-des">
-                        <h1>{opp?.title5}</h1>
-                    <p className="para-main">{opp?.short_des5}</p>
-                    <h1>Description:-</h1>
-                    <p className="para-main">{opp?.description5}</p>
-                </div>
-                <div className="">
-                    <div id="countdown" className="countdown"></div>
-                    <form action={opp?.url_opp5}>
-                    <input type="submit" value="APPLY" ></input>
-                    </form>
-                </div>
-            </div></>
-    );
+          <img src={`https://dj-front.onrender.com/media${opp?.img5}`} alt="" className="img-det d-flex flex-wrap" />
+          <div className="ph-des">
+            <h1>{opp?.title5}</h1>
+            <p className="para-main">{opp?.short_des5}</p>
+            <h1>Description:-</h1>
+            <p className="para-main">{opp?.description5}</p>
+          </div>
+          <div className="">
+            <div id="countdown" className="countdown"></div>
+            <form action={opp?.url_opp5}>
+              <input type="submit" value="APPLY" ></input>
+            </form>
+          </div>
+        </div></>
+  );
 }
 
 export default Voloid;
