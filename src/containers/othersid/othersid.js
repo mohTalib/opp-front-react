136c9dd45574cca2356edeee9d2b66cefe069658
mohTalib <107358717+mohTalib@users.noreply.github.com>
2023-10-22 01:17:38 +0300

    Update othersid.js

--- src/containers/othersid/othersid.js
+++ src/containers/othersid/othersid.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import aiox from 'aiox'; // Import aiox
+import axios from 'axios'; // Import axios
 import "./style.css";
 
 const Othersid = () => {
