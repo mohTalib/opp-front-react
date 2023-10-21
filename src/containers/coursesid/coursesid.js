29414d3e431eb65632aad6d4f7a5d4c2c7f9edbb
mohTalib <107358717+mohTalib@users.noreply.github.com>
2023-10-22 00:12:23 +0300

    Update coursesid.js

--- src/containers/coursesid/coursesid.js
+++ src/containers/coursesid/coursesid.js
@@ -1,20 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom'; 
+import { useParams } from 'react-router-dom';
+import axios from 'axios'; // Import Axios
 import "./style.css";
 
 const Coursesid = () => {
-    const { id: oppscoId } = useParams(); 
+    const { id: oppscoId } = useParams();
     const [opp, setOpp] = useState(null);
 
     const getList_Opps_course = async () => {
-        let response = await fetch(`/oppsco/${oppscoId}`)
-        let data = await response.json()
-        setOpp(data)
+        try {
+            const response = await axios.get(`https://dj-front.onrender.com/oppsco/${oppscoId}`);
+            setOpp(response.data);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
     }
 
     useEffect(() => {
         getList_Opps_course();
     }, [oppscoId]);
+
+    // Rest of your component remains the same
+    // ...
 
 
     function updateCountdown() {
@@ -54,24 +61,24 @@ const Coursesid = () => {
     }, [opp?.time1]);
     
 
-    return (
-        <><div data-aos="fade" data-aos-easing="ease-in" data-aos-duration="1000">
-        </div><div className="d-flex flex-column justify-content-center">
-                    <img src={opp?.img1} alt="" className="img-det d-flex flex-wrap" />
-                <div className="ph-des">
-                        <h1>{opp?.title1}</h1>
-                    <p className="para-main">{opp?.short_des1}</p>
-                    <h1>Description:-</h1>
-                    <p className="para-main">{opp?.description1}</p>
-                </div>
-                <div className="">
-                    <div id="countdown" className="countdown"></div>
-                    <form action={opp?.url_opp1}>
-                    <input type="submit" value="APPLY" ></input>
-                    </form>
-                </div>
-            </div></>
-    );
+return (
+    <><div data-aos="fade" data-aos-easing="ease-in" data-aos-duration="1000">
+    </div><div className="d-flex flex-column justify-content-center">
+                <img src={`/media${opp?.img1}`} alt="" className="img-det d-flex flex-wrap" />
+            <div className="ph-des">
+                    <h1>{opp?.title1}</h1>
+                <p className="para-main">{opp?.short_des1}</p>
+                <h1>Description:-</h1>
+                <p className="para-main">{opp?.description1}</p>
+            </div>
+            <div className="">
+                <div id="countdown" className="countdown"></div>
+                <form action={opp?.url_opp1}>
+                <input type="submit" value="APPLY"></input>
+                </form>
+            </div>
+        </div></>
+);
 }
 
 export default Coursesid;
