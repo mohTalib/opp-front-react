11c0ef9f02383666ac9b64b0155022874ad79b3f
mohTalib <107358717+mohTalib@users.noreply.github.com>
2023-10-22 01:26:37 +0300

    Update othersid.js

--- src/containers/othersid/othersid.js
+++ src/containers/othersid/othersid.js
@@ -21,40 +21,36 @@ const Othersid = () => {
         getList_Opps_programs();
     }, [oppsotId]);
     function updateCountdown() {
-        if (!opp?.time6) {
-            
-            document.getElementById("countdown").innerHTML = "Time Not Available";
-            return;
-        }
-
-        var currentTime = new Date().getTime();
-        var endDate = new Date(opp.time6).getTime();
-        var timeRemaining = endDate - currentTime;
-
-        if (timeRemaining <= 0) {
-            document.getElementById("countdown").innerHTML = "Not Available";
-            return; 
-        }
-
-
-        var days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-
-        document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    if (!opp?.time6) {
+      document.getElementById("countdown").innerHTML = "Time Not Available";
+      return;
     }
 
-    useEffect(() => {
-        
-        updateCountdown();
-        const interval = setInterval(updateCountdown, 1000);
+    var currentTime = new Date().getTime();
+    var endDate = new Date(opp.time6).getTime();
+    var timeRemaining = endDate - currentTime;
 
-        return () => {
-            
-            clearInterval(interval);
-        };
-    }, [opp?.time6]);
+    if (timeRemaining <= 0) {
+      document.getElementById("countdown").innerHTML = "Not Available";
+      return;
+    }
+
+    var days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  }
+
+  useEffect(() => {
+    updateCountdown();
+    const interval = setInterval(updateCountdown, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [opp?.time6]);
 
     return (
         <><div data-aos="fade" data-aos-easing="ease-in" data-aos-duration="1000">
