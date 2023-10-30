377c8bad8019d8baecbbf84664d4c445f66a9dc6
mohTalib <107358717+mohTalib@users.noreply.github.com>
2023-10-30 21:18:46 +0300

    Update volo.js

--- src/containers/volo/volo.js
+++ src/containers/volo/volo.js
@@ -19,10 +19,10 @@ function OppsCates4({ image, link }) {
     }
 
     return (
-        <div data-aos="fade" data-aos-easing="ease-in" data-aos-duration="1000">
+        <div data-aos="fade" data-aos-easing="ease-in" data-aos-duration="1000" className='mobile_size'>
             <h4 style={{ fontFamily: "Spotify Bold", fontSize: 30, color: "#fff" }}>Governmental</h4>
             <p style={{ fontSize: 14, color: "#999", fontFamily: "Spotify Bold" }}></p>
-            <div className='d-flex flex-wrap align-content-center'>
+            <div className='align-content-center padding-cards'>
                 {opps.map((opp) => (
                     <a href={`/Governmental/${opp.id}`} target="_blank" rel="noopener noreferrer" className="theanger" key={opp.id}>
                         <div className="card__container">
