18756a8b3fe5906efb12352ab06e453202130b47
mohTalib <107358717+mohTalib@users.noreply.github.com>
2023-10-31 01:45:38 +0300

    Update index.js

--- src/containers/MainView/index.js
+++ src/containers/MainView/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import DataRow from "../DataRow";
 import { data } from "../../spotify";
-
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
 import "./style.css";
 
 
@@ -11,9 +11,18 @@ function MainContainer() {
   
   return (
     <div className="main__row__container">
-      <div className="settings--icon">
-        
-      </div>
+      <a href="https://home-opp-club.com/">
+        <div className="explanation--icon">
+          <InfoOutlinedIcon
+            style={{
+              float: "right",
+              fontSize: 35,
+              color: "#fff",
+            }}
+            className="explanation--icon"
+          />
+        </div>
+      </a>
 
       {data.map((data) => {
         return (
