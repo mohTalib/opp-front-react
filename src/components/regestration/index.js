a77b6f781b107c7aca5c714809594bd9d453ec27
mohTalib <107358717+mohTalib@users.noreply.github.com>
2023-11-03 03:14:43 +0300

    Update index.js

--- src/components/regestration/index.js
+++ src/components/regestration/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import './Styles.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const RegistrationForm = () => {
   const [fullName, setFullName] = useState('');
@@ -10,39 +10,42 @@ const RegistrationForm = () => {
   const [error, setError] = useState(''); // State to hold the error message
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const validateEmail = (email) => {
+    const regex = /^[a-zA-Z]{2}(1[6-9]|2[0-3])\d{3}(@auis\.edu\.krd|@alumni\.auis\.edu\.krd)$/;
 
-    // Check if the full name and email are empty
-    if (!fullName.trim() || !email.trim()) {
-      setError('Please fill in all fields.'); // Display an error if any field is empty
-      return; // Prevent registration if any field is empty
+    if (!regex.test(email)) {
+      setError('Invalid email format. Please use the specified format.');
+      return false;
+    }
+    return true;
+  };
+
+  const handleRegistration = async () => {
+    const lowerCaseEmail = email.toLowerCase();
+
+    if (!validateEmail(lowerCaseEmail)) {
+      return;
     }
 
- const validateEmail = (email) => {
-  const regex = /^[a-zA-Z]{2}(1[6-9]|2[0-3])\d{3}(@auis\.edu\.krd|@alumni\.auis\.edu\.krd)$/;
+    try {
+      await axios.post('https://dj-front.onrender.com/register/', { full_name: fullName, email: lowerCaseEmail });
+      console.log('Registration successful');
+      navigate('/dashboard');
+    } catch (error) {
+      console.error('Registration failed', error);
+    }
+  };
 
-  if (!regex.test(email)) {
-    setError('Invalid email format. Please use the specified format.');
-    return false;
-  }
-  return true;
-};
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-const lowerCaseEmail = email.toLowerCase();
+    if (!fullName.trim() || !email.trim()) {
+      setError('Please fill in all fields.');
+      return;
+    }
 
-if (!validateEmail(lowerCaseEmail)) {
-  return;
-}
-
-try {
-  await axios.post('https://dj-front.onrender.com/register/', { full_name: fullName, email: lowerCaseEmail });
-  console.log('Registration successful');
-  navigate('/dashboard');
-} catch (error) {
-  console.error('Registration failed', error);
-}
-
+    handleRegistration();
+  };
 
   return (
     <div className="form-container">
@@ -55,17 +58,21 @@ try {
             id="fullName"
             placeholder="Full Name"
             value={fullName}
-            onChange={(e) => setFullName(e.target.value)} />
+            onChange={(e) => setFullName(e.target.value)}
+          />
           <label htmlFor="email">Write Down Your Email:</label>
           <input
             type="email"
             id="email"
             placeholder="Email"
             value={email}
-            onChange={(e) => setEmail(e.target.value)} />
+            onChange={(e) => setEmail(e.target.value)}
+          />
           {error && <p style={{ color: 'yellow' }}>{error}</p>} {/* Display error message if present */}
         </div>
-        <button className="sign" type="submit">Register</button>
+        <button className="sign" type="submit">
+          Register
+        </button>
       </form>
       <div className="social-message">
         <div className="line"></div>
