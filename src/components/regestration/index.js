e7146bd5c5f25521b91b917d7ba1eb3663399e5b
mohTalib <107358717+mohTalib@users.noreply.github.com>
2023-11-11 23:59:49 +0300

    Update index.js

--- src/components/regestration/index.js
+++ src/components/regestration/index.js
@@ -1,70 +1,48 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import { useNavigate, Link } from 'react-router-dom';
-import debounce from 'lodash.debounce';
+import { useNavigate } from 'react-router-dom';
+import './Styles.css';
+import { Link } from 'react-router-dom';
 
 const RegistrationForm = () => {
   const [fullName, setFullName] = useState('');
   const [email, setEmail] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [error, setError] = useState('');
+  const [error, setError] = useState(''); // State to hold the error message
   const navigate = useNavigate();
 
   const validateEmail = (email) => {
     const regex = /^[a-zA-Z]{2}(1[6-9]|2[0-3])\d{3}(@auis\.edu\.krd|@alumni\.auis\.edu\.krd)$/;
 
     if (!regex.test(email)) {
-      setError('Invalid email. Please use your AUIS email.');
+      setError('Invalid email. Please Your AUIS email.');
       return false;
     }
     return true;
   };
 
-  const debouncedEmailValidation = debounce(async (email) => {
-    if (email.trim() && !validateEmail(email)) {
-      setError('Invalid email. Please use your AUIS email.');
-    } else {
-      setError(''); // Clear the error if the email is valid
-    }
-  }, 300);
-
-  useEffect(() => {
-    debouncedEmailValidation(email);
-    return () => debouncedEmailValidation.cancel();
-  }, [email, debouncedEmailValidation]);
-
   const handleRegistration = async () => {
-    setIsLoading(true);
+    const lowerCaseEmail = email.toLowerCase();
+
+    if (!validateEmail(lowerCaseEmail)) {
+      return;
+    }
 
     try {
-      // Check for email validation error before attempting registration
-      if (error) {
-        console.log('Email validation error:', error);
-        return;
-      }
-
-      await axios.post('https://dj-front.onrender.com/register/', {
-        full_name: fullName,
-        email: email.toLowerCase(),
-      });
-
+      await axios.post('https://dj-front.onrender.com/register/', { full_name: fullName, email: lowerCaseEmail });
       console.log('Registration successful');
       localStorage.setItem('userStatus', 'authenticated');
       setIsLoggedIn(true);
       navigate('/dashboard');
     } catch (error) {
       console.error('Registration failed', error);
-      setError('Registration failed. Please try again.');
-    } finally {
-      setIsLoading(false);
     }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (!fullName.trim() || !email.trim() || isLoading) {
+    if (!fullName.trim() || !email.trim()) {
       setError('Please fill in all fields.');
       return;
     }
@@ -93,10 +71,10 @@ const RegistrationForm = () => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
-          {error && <p style={{ color: 'yellow' }}>{error}</p>}
+          {error && <p style={{ color: 'yellow' }}>{error}</p>} {/* Display error message if present */}
         </div>
-        <button className="sign" type="submit" disabled={isLoading}>
-          {isLoading ? 'Registering...' : 'Register'}
+        <button className="sign" type="submit">
+          Register
         </button>
       </form>
       <div className="social-message">
